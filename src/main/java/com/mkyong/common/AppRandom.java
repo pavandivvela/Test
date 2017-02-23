0de4d4eddcd2962fec9132e06caf4058222f7f09
Pavan K Divvela <pdivvela001@nam.ad.pwcinternal.com>
2017-02-23 15:35:29 -0500

    add files

--- src/main/java/com/mkyong/common/AppRandom.java
+++ src/main/java/com/mkyong/common/AppRandom.java
@@ -7,12 +7,12 @@ import java.util.UUID;
  * Generate a unique number
  *
  */
-public class App
+public class AppRandom
 {
 
     public static void main( String[] args )
     {
-        App obj = new App();
+        AppRandom obj = new AppRandom();
         System.out.println("Unique ID : " + obj.generateUniqueKey());
     }
 
